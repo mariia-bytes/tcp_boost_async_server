5364625c2b87b84955365d179105667b2933e661
mariia-bytes <mariia.burdeinaia@gmail.com>
2025-01-25 17:25:40 +0000

    client manager added

--- async_server.cpp
+++ async_server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/enable_shared_from_this.hpp>
+#include <boost/shared_ptr.hpp>
 #include <thread>
 #include <algorithm>
 #include <string>
@@ -13,13 +14,63 @@ std::string ip_address = "127.0.0.1"; // default IP address
 using namespace boost::asio;
 using ip::tcp;
 
+class Connection_Handler;
+
+
+
+/***** CLASS CLIENT MANAGER *************************************************************************/
+class Client_Manager {
+private:
+    std::map<std::string, boost::shared_ptr<Connection_Handler>> clients; // map of connected clients
+    std::mutex clients_mutex; // protection of the clients map
+   
+public:
+    // add a new client
+    void add_client(const std::string& client_id, const boost::shared_ptr<Connection_Handler>& handler) {
+        std::lock_guard<std::mutex> lock(clients_mutex);
+        clients[client_id] = handler;
+        std::cout << "\tClient added: " << client_id << "\n";
+        std::cout << "\tCurrent clients: " << clients.size() << "\n";
+    }
+
+    // remove a client
+    void remove_client(const std::string& client_id) {
+        std::lock_guard<std::mutex> lock(clients_mutex);
+
+        // check if the client is in the map
+        auto it = clients.find(client_id);
+        if (it != clients.end()) {
+            clients.erase(it);
+            std::cout << "\tClient removed from the map: " << client_id << "\n";
+        } else {
+            std::cerr << "\n\tAttempted to remove a non-existent client: " << client_id << "\n";
+        }
+        std::cout << "\tCurrent clients: " << clients.size() << "\n";
+    }
+    
+    // get the current number of clients
+    size_t get_client_count() {
+        std::lock_guard<std::mutex> lock(clients_mutex);
+        return clients.size();
+    }
+
+    // check if the client is connected
+    // haven't used it so far but let it be here for now
+    bool is_client_connected(const std::string& client_id) {
+        std::lock_guard<std::mutex> lock(clients_mutex);
+        return clients.find(client_id) != clients.end();
+    }
+};
+/*****************************************************************************************************/
+
+
 
 /************ CLASS CONNECTION HANDLER *********************************************************************/
 class Connection_Handler : public boost::enable_shared_from_this<Connection_Handler> {
 private: 
     tcp::socket connection_socket;
-    std::string client_id; // unique adentifier for the client
-    // Server& server;
+    std::string client_id; // unique identifier for the clien
+    Client_Manager& client_manager; 
     std::string message = "Hello from Server!\n"; // greeting message to the server
     boost::asio::streambuf read_buffer; // dynamic buffer to read from the socket
 public:
@@ -27,22 +78,37 @@ public:
 
 
 public:
-    Connection_Handler(boost::asio::io_context& io_context) 
-        : connection_socket(io_context) {}
-
-    // get client unique identifier
-    std::string get_client_id() const {
-        return client_id;
-    }
+    Connection_Handler(boost::asio::io_context& io_context, Client_Manager& manager) 
+        : connection_socket(io_context), client_manager(manager) {}
         
     // create a shared pointer
-    static pointer create(boost::asio::io_context& io_context) {
-        return pointer(new Connection_Handler(io_context));
+    static pointer create(boost::asio::io_context& io_context, Client_Manager& manager) {
+        return pointer(new Connection_Handler(io_context, manager));
     }
 
     // access socket
     tcp::socket& socket() {
         return connection_socket;
+    }   
+
+    void start() {
+        // capture the ownership
+        auto self = shared_from_this();
+
+        retrive_client_id();
+        client_manager.add_client(client_id, shared_from_this());
+
+        // send an initial message
+        connection_socket.async_write_some(
+            boost::asio::buffer(message),
+            [self](const boost::system::error_code& err, std::size_t bytes_transferred) {
+                if (!err) {
+                    std::cout << "\nMessage sent to client: " << self->message;
+                } else {
+                    std::cerr << "Write error: " << err.message() << "\n";
+                }
+            });
+        do_read();
     }
 
     void retrive_client_id() {
@@ -56,24 +122,22 @@ public:
         }
     }
 
-    void start() {
-        // capture shared ownership of the handler
-        auto self = shared_from_this();
-
-        retrive_client_id();
-
-        // send an initial message
-        connection_socket.async_write_some(
-            boost::asio::buffer(message),
-            [self](const boost::system::error_code& err, std::size_t bytes_transferred) {
-                if (!err) {
-                    std::cout << "\nMessage sent to client: " << self->message;
-                } else {
-                    std::cerr << "Write error: " << err.message() << "\n";
-                }
-            });
-        
-        do_read();
+    ~Connection_Handler() {
+        if (connection_socket.is_open()) {
+            boost::system::error_code ec;
+            connection_socket.cancel(ec); // Cancel pending operations
+            if (ec) {
+                std::cerr << "Error canceling socket: " << ec.message() << "\n";
+            }
+        }
+        /* 
+        // can't say why I don't have to do it but the valgrind tells me I don't
+        // client_manager.remove_client(client_id);
+        if (!client_id.empty()) {
+            client_manager.remove_client(client_id);
+        }
+        std::cout << "Connection_Handler destroyed for client: " << client_id << "\n";
+        */
     }
     
 
@@ -108,17 +172,32 @@ private:
                 std::cout << "Client> " << data << "\n";
             }
 
+            // clear the buffer
             read_buffer.consume(bytes_transferred);
+
+            // continue reading
             do_read();
         } else if (err == boost::asio::error::eof) {
+            // client disconnected gracefully
             std::cout << "\nConnection closed by the client: " << client_id << "\n";
-            // std::cout << "Current clients: " << need to make something
             connection_socket.close();
-            // server.remove_client(client_id); // notify the server
+            client_manager.remove_client(client_id);
+            std::cout << "Current clients: " << client_manager.get_client_count() << "\n";
+            
+        } else if (err == boost::asio::error::operation_aborted) {
+            // (possibly) the server shut down
+            std::cerr << "Operation aborted for client: " << client_id << "\n";
+        /*
+        } else if (err == boost::asio::error::resouce_unavailable_try_again) {
+            // temporary issue, log and retry
+            std::cerr << "Temporary resource issue for client: " << client_id << "\n";
+            do_read();
+        */
         } else {
+            // all other errors are treated as non-recoverable
             std::cerr << "Read error: " << err.message() << "\n";
             connection_socket.close();
-            // server.remove_client(client_id); // notify the server
+            client_manager.remove_client(client_id);
         }
     }
 };
@@ -128,14 +207,13 @@ private:
 class Server {
 private:
     tcp::acceptor server_acceptor;
-    std::map<std::string, Connection_Handler::pointer> clients; // map of connected clients
-    std::mutex clients_mutex; // to protect clients map
+    Client_Manager client_manager; 
     std::atomic<bool> is_waiting = true; // flag to control print "waiting..."
     
     void start_accept() {
-        auto& io_context = static_cast<boost::asio::io_context&>(server_acceptor.get_executor().context());
+        boost::asio::io_context& io_context = static_cast<boost::asio::io_context&>(server_acceptor.get_executor().context());
         
-        Connection_Handler::pointer connection = Connection_Handler::create(io_context);
+        auto connection = Connection_Handler::create(io_context, client_manager);
 
         server_acceptor.async_accept(
             connection->socket(),
@@ -157,24 +235,12 @@ public:
     void handle_accept(Connection_Handler::pointer connection, const boost::system::error_code& err) {
         if (!err) {
             connection->start();
-            std::string client_id = connection->get_client_id();
-            // lock before modifying the map
-            std::lock_guard<std::mutex> lock(clients_mutex);
-            clients[client_id] = connection;
-            std::cout << "Current clients: " << clients.size() << "\n";
         }
         start_accept();
     }
-
-    // remove client when it's disconnected
-    void remove_client(const std::string& client_id) {
-        std::lock_guard<std::mutex> lock(clients_mutex);
-        clients.erase(client_id);
-        std::cout << "\nClient disconnected: " << client_id << "\n";
-        std::cout << "Current clients: " << clients.size() << "\n";
-    }
 };
 /*****************************************************************************************************/
+
 
 
 int main(int argc, char* argv[]) {
@@ -201,7 +267,7 @@ int main(int argc, char* argv[]) {
         // handling signal for graceful shutdown
         boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
         signals.async_wait([&io_context](const boost::system::error_code&, int) {
-            std::cout << "\nShutting down the server..." << std::endl;
+            std::cout << "\n\nShutting down the server..." << std::endl;
             io_context.stop();
         });
 
